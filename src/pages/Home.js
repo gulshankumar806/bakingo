9a407a331551d02b0b6213e3f6e0438d4346e843
gulshankumar <gulshan.kumar@saffrontech.net>
2021-05-20 10:51:24 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import Gifts from '../gifts/Gifts.js';
 
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
 const Home = () => {
+
+    const [nav1, setNav1] = useState();
+    const [nav2, setNav2] = useState();
+
+    const settings = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        infinite: true,
+        asNavFor: '.testimonial-txt'
+      };
+
+      const settings2 = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        fade: false,
+        asNavFor: '.testimonial-img',
+        dots: true
+      };
+
   return (
     <div className="Home">
 
@@ -52,72 +81,53 @@ const Home = () => {
               </div>
           </div>
       </div>
-      
-      <div className="more-gift-sec">
-          <div className="container">
-              <div className="more-gift-wp">
-                  <div className="ttl-wp">
-                      <h2 className="sec-ttl text-center">More Gifts for Loved One</h2>
-                      <img src="image/heart-dvdr.png" alt="" className="dvdr-img" />
-                  </div>
-
-                  <div className="gift-item-wp">
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-1.png" alt="" />
-                              <h2>Flowers</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-2.png" alt="" />
-                              <h2>Cakes</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-3.png" alt="" />
-                              <h2>Plants</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-4.png" alt="" />
-                              <h2>Gifts</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-5.png" alt="" />
-                              <h2>Combos</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-6.png" alt="" />
-                              <h2>Hampers</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-7.png" alt="" />
-                              <h2>Personalised</h2>
-                          </a>
-                      </div>
-                      <div className="gift-item">
-                          <a href="javascript:void(0);">
-                              <img src="../../images/prod-8.png" alt="" />
-                              <h2>Lamps</h2>
-                          </a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-
+     
 
       <Gifts />
+
+
+    <div className="testimonial-sec">
+        <div className="container">
+            <div className="testimonial-wp">
+            
+                <div className="testimonial-img">
+                    <Slider {...settings} asNavFor={nav2} ref={(slider1) => setNav1(slider1)}>
+                        <div>
+                            <figure>
+                                <img src="../../images/testimonial-img-1.png" alt="" />
+                            </figure>
+                        </div>
+                        <div>                            
+                            <figure>
+                                <img src="../../images/testimonial-img-2.png" alt="" />
+                            </figure>
+                        </div>
+                    </Slider>
+                </div>
+                <div className="testimonial-cont">                
+                    <div className="ttl-wp">
+                        <h2 className="sec-ttl text-center">Confession Stories</h2>
+                        <img src="image/heart-dvdr-sht.png" alt="" className="dvdr-img" />
+                    </div>
+                    <Slider  {...settings2} asNavFor={nav1}
+        ref={(slider2) => setNav2(slider2)} className="testimonial-txt">
+                        <div>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+                            </p>
+                            <a href="javascript:void(0);" className="read-btn">Read Aman's Love Story</a>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+                            </p>
+                        </div>
+                    </Slider>
+                </div>
+            </div>
+        </div>
+    </div>
+
     </div>
   );
 }
